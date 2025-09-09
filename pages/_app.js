e01df4012b7c682bce765649cd702ba036d457af
ntuyxd05-ux <ntuyxd05@gmail.com>
2025-09-10 02:13:48 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "../styles/globals.css";  // pakai relative path
+import "../styles/globals.css";  // pakai path relatif, BUKAN '@/...'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
